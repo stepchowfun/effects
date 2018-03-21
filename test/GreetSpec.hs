0e981a39a13a65b4665ab7e246e7fafb7688e17b
Stephan Boyer <stephan@stephanboyer.com>
2018-03-21 05:32:04 -0700

    Remove QuickCheck since it's not being used

--- test/GreetSpec.hs
+++ test/GreetSpec.hs
@@ -2,7 +2,5 @@ module GreetSpec (greetSpec) where
 
 import Test.Hspec (Spec, describe, it, pending)
 
--- The QuickCheck specs
-
 greetSpec :: Spec
 greetSpec = describe "error" $ it "should be correct" $ pending
