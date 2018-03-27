4348623bc2c997127ad1a7714f38f19e9eeee4f4
Stephan Boyer <stephan@stephanboyer.com>
2018-03-26 22:22:38 -0700

    Add another example: monad transformers

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,11 @@
 module Main (main) where
 
 import BigMonad
+import MonadTransformers
 
 main :: IO ()
 main = do
   putStrLn "Big monad:\n"
   BigMonad.ioProgram
+  putStrLn "Monad transformers:\n"
+  MonadTransformers.ioProgram
--- test/MainSpec.hs
+++ test/MainSpec.hs
@@ -1,6 +1,8 @@
 import BigMonadSpec
+import MonadTransformersSpec
 import Test.Hspec (hspec)
 
 main :: IO ()
 main = hspec $ do
   BigMonadSpec.spec
+  MonadTransformersSpec.spec
