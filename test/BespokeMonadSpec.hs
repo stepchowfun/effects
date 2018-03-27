3787687daf913989ca385fb99e9ec763af2d6f56
Stephan Boyer <stephan@stephanboyer.com>
2018-03-26 23:25:18 -0700

    Make the tests more DRY

--- test/BespokeMonadSpec.hs
+++ test/BespokeMonadSpec.hs
@@ -1,6 +1,7 @@
 module BespokeMonadSpec (spec) where
 
 import BespokeMonad (Computation(..), program, run)
+import ExpectedOutput (expectedOutput)
 import System.Random (mkStdGen)
 import Test.Hspec (Spec, describe, it, shouldBe)
 
@@ -8,4 +9,4 @@ spec :: Spec
 spec = describe "Bespoke monad" $ do
   it "should produce the correct output" $ do
     let (_, _, s, _) = run program (mkStdGen 0) 0
-    s `shouldBe` "0\n3\n6\n9\n17\n17\n24\n25\n26\n27\n"
+    s `shouldBe` expectedOutput
