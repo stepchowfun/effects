27c38dfb1cbe7f39b50441829cb550369f97891e
Stephan Boyer <stephan@stephanboyer.com>
2018-04-21 14:30:48 -0700

    Add HLint

--- test/FreeMonadSpec.hs
+++ test/FreeMonadSpec.hs
@@ -8,8 +8,7 @@ import FreeMonad (interpret, program)
 import Test.Hspec (Spec, describe, it, shouldBe)
 
 spec :: Spec
-spec = describe "Free monad" $ do
-  it "should produce the correct output" $ do
-    let (((_, s), _), _) =
-          runRand (runStateT (runWriterT (interpret program)) 0) (mkStdGen 0)
-    s `shouldBe` expectedOutput
+spec = describe "Free monad" $ it "should produce the correct output" $ do
+  let (((_, s), _), _) =
+        runRand (runStateT (runWriterT (interpret program)) 0) (mkStdGen 0)
+  s `shouldBe` expectedOutput
--- test/MonadTransformersSpec.hs
+++ test/MonadTransformersSpec.hs
@@ -8,8 +8,8 @@ import MonadTransformers (program)
 import Test.Hspec (Spec, describe, it, shouldBe)
 
 spec :: Spec
-spec = describe "Monad transformers" $ do
-  it "should produce the correct output" $ do
+spec =
+  describe "Monad transformers" $ it "should produce the correct output" $ do
     let (((_, s), _), _) =
           runRand (runStateT (runWriterT program) 0) (mkStdGen 0)
     s `shouldBe` expectedOutput
