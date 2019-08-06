20f8418a836d1790b523e42fd11c5f825291649b
Katrin Leinweber <kalei@posteo.de>
2019-08-06 20:56:09 +0200

    Hyperlink DOIs to preferred resolver

--- src/ExtensibleEffects.hs
+++ src/ExtensibleEffects.hs
@@ -18,12 +18,12 @@ module ExtensibleEffects
  - [1] Oleg Kiselyov, Amr Sabry, and Cameron Swords. 2013. Extensible effects:
  -     an alternative to monad transformers. In Proceedings of the 2013 ACM
  -     SIGPLAN symposium on Haskell (Haskell '13). ACM, New York, NY, USA,
- -     59-70. DOI=http://dx.doi.org/10.1145/2503778.2503791
+ -     59-70. DOI=https://doi.org/10.1145/2503778.2503791
  -
  - [2] Oleg Kiselyov and Hiromi Ishii. 2015. Freer monads, more extensible
  -     effects. In Proceedings of the 2015 ACM SIGPLAN Symposium on Haskell
  -     (Haskell '15). ACM, New York, NY, USA, 94-105.
- -     DOI=http://dx.doi.org/10.1145/2804302.2804319
+ -     DOI=https://doi.org/10.1145/2804302.2804319
  -}
 import Control.Eff (Eff, Member, run)
 import Control.Eff.Extend (handle_relay_s, send)
